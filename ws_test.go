1947eb3f16260504f0042034458f9bd5fd83a0e8
benitogf <benitogf@outlook.com>
2019-04-29 17:04:32 +0800

    collection patch

--- ws_test.go
+++ ws_test.go
@@ -163,7 +163,8 @@ func TestWsBroadcast(t *testing.T) {
 	readCount := 0
 	got1 := ""
 	got2 := ""
-	cache := ""
+	cache1 := ""
+	cache2 := ""
 
 	go func() {
 		for {
@@ -181,7 +182,17 @@ func TestWsBroadcast(t *testing.T) {
 				err = c1.Close()
 				require.NoError(t, err)
 			}
+			if readCount == 1 {
+				patch, err := jsonpatch.DecodePatch([]byte(event.Data))
+				require.NoError(t, err)
+
+				modified, err := patch.Apply([]byte(cache1))
+				require.NoError(t, err)
+
+				cache1 = string(modified)
+			}
 			if readCount == 0 {
+				cache1 = event.Data
 				err = c1.WriteMessage(websocket.TextMessage, []byte("{"+
 					"\"op\": \"del\","+
 					"\"index\": \"2\""+
@@ -218,7 +229,7 @@ func TestWsBroadcast(t *testing.T) {
 			err = c2.Close()
 			require.NoError(t, err)
 		} else {
-			cache = event.Data
+			cache2 = event.Data
 			app.console.Log("writing from c2")
 			err = c2.WriteMessage(websocket.TextMessage, []byte("{"+
 				"\"index\": \"1\","+
@@ -239,20 +250,27 @@ func TestWsBroadcast(t *testing.T) {
 		mutex.RLock()
 	}
 	mutex.RUnlock()
-	patch, err := jsonpatch.DecodePatch([]byte(got2))
+
+	patch1, err := jsonpatch.DecodePatch([]byte(got1))
 	require.NoError(t, err)
 
-	modified, err := patch.Apply([]byte(cache))
+	modified1, err := patch1.Apply([]byte(cache1))
+	require.NoError(t, err)
+
+	patch2, err := jsonpatch.DecodePatch([]byte(got2))
+	require.NoError(t, err)
+
+	modified2, err := patch2.Apply([]byte(cache2))
 	require.NoError(t, err)
 
 	opts := jsondiff.DefaultConsoleOptions()
 	result, _ := jsondiff.Compare(
-		[]byte(got1),
-		[]byte("["+string(modified)+"]"),
+		modified1,
+		[]byte("["+string(modified2)+"]"),
 		&opts)
 
-	app.console.Log("patch: ", got2)
-	app.console.Log(got1, "["+string(modified)+"]")
+	app.console.Log("patches: ", got1, got2)
+	app.console.Log("merged: ", string(modified1), "["+string(modified2)+"]")
 
 	require.Equal(t, result, jsondiff.FullMatch)
 }
