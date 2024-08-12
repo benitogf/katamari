9a6aea021554538e9969bb30d5ee19a055cca8cb
root <root@DESKTOP-5PMS49A>
2024-08-12 18:04:25 +0800

    cancel context, generic callback

--- client/client.go
+++ client/client.go
@@ -1,89 +1,158 @@
 package client
 
 import (
+	"context"
+	"encoding/json"
+	"net/http"
 	"net/url"
+	"sync"
+	"sync/atomic"
 	"time"
 
-	"github.com/benitogf/katamari"
 	"github.com/benitogf/katamari/key"
 	"github.com/benitogf/katamari/messages"
 	"github.com/benitogf/katamari/objects"
 	"github.com/gorilla/websocket"
 )
 
-// https://go.googlesource.com/proposal/+/refs/heads/master/design/43651-type-parameters.md#No-parameterized-methods
-type Message interface {
-	objects.Object | []objects.Object
+// var DEBUG = false
+
+// func debug(v ...any) {
+// 	if DEBUG {
+// 		log.Println(v...)
+// 	}
+// }
+
+var HandshakeTimeout time.Duration = time.Second * 2
+
+type Meta[T any] struct {
+	Created int64  `json:"created"`
+	Updated int64  `json:"updated"`
+	Index   string `json:"index"`
+	Data    T      `json:"data"`
 }
+type OnMessageCallback[T any] func([]Meta[T])
 
-// type onMessageCallback [T Message]func(*katamari.Server, T)
-// type onMessageCallback func(*katamari.Server, interface{})
-
-type ListCallback func(*katamari.Server, []objects.Object)
-type Callback func(*katamari.Server, objects.Object)
-
-type Reader struct {
-	ListCallback ListCallback
-	Callback     Callback
-}
-
-func Subscribe(server *katamari.Server, protocol, host, path string, reader Reader) {
+func Subscribe[T any](ctx context.Context, protocol, host, path string, callback OnMessageCallback[T]) {
 	retryCount := 0
 	cache := ""
 	lastPath := key.LastIndex(path)
 	isList := lastPath == "*"
+	closingTime := atomic.Bool{}
+	wsURL := url.URL{Scheme: protocol, Host: host, Path: path}
+	muWsClient := sync.Mutex{}
+	var wsClient *websocket.Conn
+	_handShakeTimeout := HandshakeTimeout
+
+	go func(ct *atomic.Bool) {
+		<-ctx.Done()
+		ct.Swap(true)
+		muWsClient.Lock()
+		defer muWsClient.Unlock()
+		if wsClient == nil {
+			// debug("subscribe client closing but no connection to close", host, path, ctx.Err())
+			return
+		}
+		// debug("subscribe client closing", host, path, ctx.Err())
+		wsClient.Close()
+	}(&closingTime)
+
 	for {
-		wsURL := url.URL{Scheme: protocol, Host: host, Path: path}
-		wsClient, _, err := websocket.DefaultDialer.Dial(wsURL.String(), nil)
+		var err error
+		quickDial := &websocket.Dialer{
+			Proxy:            http.ProxyFromEnvironment,
+			HandshakeTimeout: _handShakeTimeout,
+		}
+
+		muWsClient.Lock()
+		wsClient, _, err = quickDial.Dial(wsURL.String(), nil)
 		if wsClient == nil || err != nil {
-			server.Console.Err("failed websocket dial ", err)
+			muWsClient.Unlock()
+			// debug("failed websocket dial ", err)
 			time.Sleep(2 * time.Second)
 			continue
 		}
+		muWsClient.Unlock()
+		// debug("client connection stablished", host, path)
+
 		for {
 			_, message, err := wsClient.ReadMessage()
 			if err != nil || message == nil {
-				server.Console.Err("failed websocket connection ", err)
+				// debug("failed websocket read connection ", err)
 				wsClient.Close()
 				break
 			}
 
+			result := []Meta[T]{}
 			if isList {
 				var objs []objects.Object
 				cache, objs, err = messages.PatchList(message, cache)
 				if err != nil {
-					server.Console.Err("fail to parse message from websocket", err)
+					// debug("fail to parse message from websocket", err)
 					break
 				}
+				for _, obj := range objs {
+					var item T
+					err = json.Unmarshal([]byte(obj.Data), &item)
+					if err != nil {
+						// debug("fail to unmarshal data from websocket", err)
+						continue
+					}
+					result = append(result, Meta[T]{
+						Created: obj.Created,
+						Updated: obj.Updated,
+						Index:   obj.Index,
+						Data:    item,
+					})
+				}
 				retryCount = 0
-				reader.OnListUpdate(server, objs)
+				callback(result)
 				continue
 			}
 
 			var obj objects.Object
 			cache, obj, err = messages.Patch(message, cache)
 			if err != nil {
-				server.Console.Err("fail to parse message from websocket", err)
+				// debug("fail to parse message from websocket", err)
 				break
 			}
 
+			var item T
+			err = json.Unmarshal([]byte(obj.Data), &item)
+			if err != nil {
+				// debug("fail to unmarshal data from websocket", err)
+				break
+			}
+			result = append(result, Meta[T]{
+				Created: obj.Created,
+				Updated: obj.Updated,
+				Index:   obj.Index,
+				Data:    item,
+			})
 			retryCount = 0
-			reader.OnUpdate(server, obj)
+			callback(result)
 		}
+
+		bye := closingTime.Load()
+		if bye {
+			// debug("skip reconnection, client closing...", host, path)
+			break
+		}
+
 		retryCount++
 		if retryCount < 30 {
-			server.Console.Err("reconnecting websocket...", err)
+			// debug("reconnecting websocket...", host, path, err)
 			time.Sleep(300 * time.Millisecond)
 			continue
 		}
 
 		if retryCount < 100 {
-			server.Console.Err("reconnecting websocket in 2 seconds...", err)
+			// debug("reconnecting websocket in 2 seconds...", host, path, err)
 			time.Sleep(2 * time.Second)
 			continue
 		}
 
-		server.Console.Err("reconnecting websocket in 10 seconds...", err)
+		// debug("reconnecting websocket in 10 seconds...", err)
 		time.Sleep(10 * time.Second)
 	}
 }
---
+++ client/client_test.go
@@ -0,0 +1,164 @@
+package client_test
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"os"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/benitogf/katamari"
+	"github.com/benitogf/katamari/client"
+	"github.com/benitogf/katamari/key"
+	"github.com/pkg/expect"
+	"github.com/stretchr/testify/require"
+)
+
+type Device struct {
+	Name string `json:"name"`
+}
+
+func createDevice(t *testing.T, server *katamari.Server, name string) {
+	newKey := key.Build("devices/*")
+	newDevice := Device{
+		Name: name,
+	}
+
+	newDeviceData, err := json.Marshal(newDevice)
+	require.NoError(t, err)
+
+	newDeviceDataEncoded := base64.StdEncoding.EncodeToString(newDeviceData)
+	server.Storage.Set(newKey, newDeviceDataEncoded)
+}
+
+func TestClientList(t *testing.T) {
+	server := katamari.Server{}
+	server.Silence = true
+	server.Start("localhost:0")
+	defer server.Close(os.Interrupt)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(1)
+	go client.Subscribe(ctx, "ws", server.Address, "devices/*",
+		func(devices []client.Meta[Device]) {
+			if len(devices) > 0 {
+				require.Equal(t, "device "+strconv.Itoa(len(devices)-1), devices[0].Data.Name)
+			}
+			wg.Done()
+		})
+
+	wg.Wait()
+
+	for i := range 5 {
+		wg.Add(1)
+		createDevice(t, &server, "device "+strconv.Itoa(i))
+		wg.Wait()
+	}
+}
+
+func TestClientClose(t *testing.T) {
+	server := katamari.Server{}
+	server.Silence = true
+	server.Start("localhost:0")
+	defer server.Close(os.Interrupt)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	// client.DEBUG = true
+	go client.Subscribe(ctx, "ws", server.Address, "devices/*",
+		func(devices []client.Meta[Device]) {
+			wg.Done()
+		})
+	wg.Wait()
+
+	cancel()
+	time.Sleep(10 * time.Millisecond) // wait for the connection to be closed
+	createDevice(t, &server, "device null")
+	time.Sleep(100 * time.Millisecond) // wait to verify that the update is not received
+}
+
+func TestClientCloseWhileReconnecting(t *testing.T) {
+	server := katamari.Server{}
+	server.Silence = true
+	server.Start("localhost:0")
+	defer server.Close(os.Interrupt)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	// client.DEBUG = false
+	go client.Subscribe(ctx, "ws", server.Address, "devices/*",
+		func(devices []client.Meta[Device]) {
+			wg.Done()
+		})
+	wg.Wait()
+
+	// close server and wait for the client to start reconnection attempts
+	server.Close(os.Interrupt)
+	time.Sleep(1 * time.Second)
+
+	// cancel while reconnecting in progress
+	cancel()
+	createDevice(t, &server, "device null")
+	time.Sleep(1 * time.Second)
+}
+
+func TestClientCloseWithoutConnection(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	client.HandshakeTimeout = 10 * time.Millisecond
+	go client.Subscribe(ctx, "ws", "notAnIP", "devices/*",
+		func(devices []client.Meta[Device]) {
+			expect.True(false)
+		})
+
+	// wait for retries to stablish connection
+	time.Sleep(200 * time.Millisecond)
+
+	// cancel while trying to connect
+	cancel()
+	time.Sleep(200 * time.Millisecond)
+}
+
+func TestClientListCallbackCurry(t *testing.T) {
+	server := katamari.Server{}
+	server.Silence = true
+	server.Start("localhost:0")
+	defer server.Close(os.Interrupt)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	wg := sync.WaitGroup{}
+
+	messagesCount := 0
+	devicesCallbacker := func() client.OnMessageCallback[Device] {
+		return func(devices []client.Meta[Device]) {
+			messagesCount++
+			if len(devices) > 0 {
+				require.Equal(t, "device "+strconv.Itoa(len(devices)-1), devices[0].Data.Name)
+			}
+			wg.Done()
+		}
+	}
+
+	wg.Add(1)
+	go client.Subscribe(ctx, "ws", server.Address, "devices/*", devicesCallbacker())
+
+	wg.Wait()
+
+	const NUM_DEVICES = 5
+	for i := range NUM_DEVICES {
+		wg.Add(1)
+		createDevice(t, &server, "device "+strconv.Itoa(i))
+		wg.Wait()
+	}
+
+	require.Equal(t, NUM_DEVICES+1, messagesCount)
+}
